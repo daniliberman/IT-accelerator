9d062a6a294999b4f10865d2b8c75585d0998aed
DANIELLA LIBERMAN <dliberman@ar0fvfxf1a9hv2j.ml.com>
2018-11-28 11:54:46 -0300

    arreglo persona

--- Persona.java
+++ Persona.java
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 public class Persona {
   private int otroId;
 
   public Persona(int id) {
     otroId = id;
   }
+=======
+Public class Persona {
+  
+>>>>>>> creo la clase persona
 }
